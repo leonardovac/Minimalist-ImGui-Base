leonardovac/Minimalist-ImGui-Base

commit 691be77deaa3a815483cf5d1e6c2ee7a56bd0300

feat: Add Quill as logging library.

src/base.cpp
@@ -4,7 +4,9 @@
 
 #include <SafetyHook/Wrapper.hpp>
 #include <ScreenCleaner/ScreenCleaner.h>
+
 #include "hooks.h"
+#include "misc/logger.h"
 #include "ui/overlay.h"
 
 namespace
@@ -53,7 +55,7 @@ namespace
 
 		if (!Overlay::TryAllPresentMethods())
 		{
-			MessageBoxA(nullptr, "Couldn't hook game rendering.", nullptr, 0);
+			LOG_CRITICAL("Couldn't hook game rendering.");
 		}
 	}
 }
@@ -61,6 +63,11 @@ namespace
 ScreenCleaner screenCleaner(&Overlay::bEnabled);
 void MainThread()
 {
+#if LOGGING_ENABLED
+	ConsoleManager::create_console();
+	SetupQuill("log.txt");
+#endif
+
 	Hooks::SetupAllHooks();
 	std::thread(HookPresent).detach();
 	if (screenCleaner.Init()) screenCleaner.Enable();

src/misc/logger.cpp
@@ -1,0 +1,87 @@
+﻿#include "logger.h"
+#if LOGGING_ENABLED
+#include <quill/Frontend.h>
+#include <quill/sinks/ConsoleSink.h>
+#include <quill/sinks/FileSink.h>
+
+// Static member definition
+bool ConsoleManager::allocated_ = false;
+
+// Global logger definition
+quill::Logger* globalLogger = nullptr;
+
+void ConsoleManager::create_console()
+{
+    if (allocated_) return;
+
+    if (AllocConsole())
+    {
+        if (IsValidCodePage(CP_UTF8))
+        {
+            SetConsoleCP(CP_UTF8);
+            SetConsoleOutputCP(CP_UTF8);
+        }
+
+        if (const HANDLE hStd = GetStdHandle(STD_OUTPUT_HANDLE); hStd != INVALID_HANDLE_VALUE)
+        {
+            SetConsoleMode(hStd, ENABLE_PROCESSED_OUTPUT | ENABLE_WRAP_AT_EOL_OUTPUT | ENABLE_VIRTUAL_TERMINAL_PROCESSING);
+        }
+
+        SetConsoleCtrlHandler(nullptr, TRUE);
+
+        if (freopen_s(reinterpret_cast<FILE**>(stdout), "CONOUT$", "w", stdout) == 0 &&
+            freopen_s(reinterpret_cast<FILE**>(stderr), "CONOUT$", "w", stderr) == 0)
+        {
+            std::ios::sync_with_stdio();
+            SetConsoleTitleW(L"Log Console");
+            allocated_ = true;
+        }
+    }
+}
+
+void ConsoleManager::destroy_console()
+{
+    if (!allocated_) return;
+
+    FreeConsole();
+    allocated_ = false;
+}
+
+void SetupQuill(char const* logFile)
+{
+    quill::Backend::start();
+
+    quill::ConsoleSinkConfig consoleSinkConfig{};
+    quill::ConsoleSinkConfig::Colours colours;
+    colours.assign_colour_to_log_level(quill::LogLevel::Notice, quill::ConsoleSinkConfig::Colours::blue);
+	consoleSinkConfig.set_colours(colours);
+
+    auto consoleSink = quill::Frontend::create_or_get_sink<quill::ConsoleSink>("console_sink", consoleSinkConfig);
+    auto fileSink = quill::Frontend::create_or_get_sink<quill::FileSink>(
+        logFile,
+        []
+        {
+            quill::FileSinkConfig cfg;
+            cfg.set_open_mode('a');
+            cfg.set_filename_append_option(quill::FilenameAppendOption::StartDate);
+            return cfg;
+        }(),
+        quill::FileEventNotifier{});
+
+    globalLogger = quill::Frontend::create_or_get_logger(
+        "global",
+        { std::move(consoleSink), std::move(fileSink) },
+        quill::PatternFormatterOptions
+        {
+            "%(time) [%(thread_id:<5)] %(short_source_location:<16) %(log_level:<9) [%(caller_function)] %(message)",
+            "%H:%M:%S"
+        }
+    );
+
+#ifdef _DEBUG
+    globalLogger->set_log_level(quill::LogLevel::Debug);
+#endif
+
+    quill::Frontend::preallocate();
+}
+#endif

src/misc/logger.h
@@ -1,0 +1,50 @@
+﻿#pragma once
+#define ENABLE_LOGGING true
+
+#if ENABLE_LOGGING || defined(_DEBUG)
+#define LOGGING_ENABLED 1
+#else
+#define LOGGING_ENABLED 0
+#endif
+
+#if LOGGING_ENABLED
+#define _CRT_SECURE_NO_WARNINGS
+#define QUILL_DISABLE_NON_PREFIXED_MACROS
+
+#include <quill/Backend.h>
+#include <quill/Logger.h>
+#include <quill/LogMacros.h>
+
+
+
+class ConsoleManager
+{
+    static bool allocated_;
+
+public:
+    static void create_console();
+    static void destroy_console();
+};
+
+void SetupQuill(char const* logFile);
+
+extern quill::Logger* globalLogger;
+
+#define LOG_IMPL(level, fmt, ...) QUILL_LOG_##level(globalLogger, fmt, ##__VA_ARGS__)
+#define LOG_IMPL_DYNAMIC(log_level, fmt, ...) QUILL_LOG_DYNAMIC(globalLogger, log_level, fmt, ##__VA_ARGS__)
+#else
+#define LOG_IMPL(level, fmt, ...) (void)0
+#define LOG_IMPL_DYNAMIC(log_level, fmt, ...) (void)0
+#endif
+
+#define LOG_TRACE_L3(fmt, ...) LOG_IMPL(TRACE_L3, fmt, ##__VA_ARGS__)
+#define LOG_TRACE_L2(fmt, ...) LOG_IMPL(TRACE_L2, fmt, ##__VA_ARGS__)
+#define LOG_TRACE_L1(fmt, ...) LOG_IMPL(TRACE_L1, fmt, ##__VA_ARGS__)
+#define LOG_DEBUG(fmt, ...) LOG_IMPL(DEBUG, fmt, ##__VA_ARGS__)
+#define LOG_INFO(fmt, ...) LOG_IMPL(INFO, fmt, ##__VA_ARGS__)
+#define LOG_NOTICE(fmt, ...) LOG_IMPL(NOTICE, fmt, ##__VA_ARGS__)
+#define LOG_WARNING(fmt, ...) LOG_IMPL(WARNING, fmt, ##__VA_ARGS__)
+#define LOG_ERROR(fmt, ...) LOG_IMPL(ERROR, fmt, ##__VA_ARGS__)
+#define LOG_CRITICAL(fmt, ...) LOG_IMPL(CRITICAL, fmt, ##__VA_ARGS__)
+#define LOG_BACKTRACE(fmt, ...) LOG_IMPL(BACKTRACE, fmt, ##__VA_ARGS__)
+#define LOG_DYNAMIC(log_level, fmt, ...) LOG_IMPL_DYNAMIC(log_level, fmt, ##__VA_ARGS__)

version.vcxproj
@@ -110,7 +110,7 @@
     <ClCompile>
       <WarningLevel>Level3</WarningLevel>
       <SDLCheck>true</SDLCheck>
-      <PreprocessorDefinitions>WIN32;_DEBUG;VERSION_EXPORTS;_WINDOWS;_USRDLL;WIN32_LEAN_AND_MEAN;NOMINMAX;NOGDI;NOSERVICE;%(PreprocessorDefinitions)</PreprocessorDefinitions>
+      <PreprocessorDefinitions>WIN32;_DEBUG;VERSION_EXPORTS;_WINDOWS;_USRDLL;WIN32_LEAN_AND_MEAN;NOGDI;NOMINMAX;NOSERVICE;%(PreprocessorDefinitions)</PreprocessorDefinitions>
       <ConformanceMode>true</ConformanceMode>
       <PrecompiledHeader>NotUsing</PrecompiledHeader>
       <PrecompiledHeaderFile>pch.h</PrecompiledHeaderFile>
@@ -130,7 +130,7 @@
       <FunctionLevelLinking>true</FunctionLevelLinking>
       <IntrinsicFunctions>true</IntrinsicFunctions>
       <SDLCheck>true</SDLCheck>
-      <PreprocessorDefinitions>WIN32;NDEBUG;VERSION_EXPORTS;_WINDOWS;_USRDLL;WIN32_LEAN_AND_MEAN;NOMINMAX;NOGDI;NOSERVICE;%(PreprocessorDefinitions)</PreprocessorDefinitions>
+      <PreprocessorDefinitions>WIN32;NDEBUG;VERSION_EXPORTS;_WINDOWS;_USRDLL;WIN32_LEAN_AND_MEAN;NOGDI;NOMINMAX;NOSERVICE;%(PreprocessorDefinitions)</PreprocessorDefinitions>
       <ConformanceMode>true</ConformanceMode>
       <PrecompiledHeader>NotUsing</PrecompiledHeader>
       <PrecompiledHeaderFile>pch.h</PrecompiledHeaderFile>
@@ -152,7 +152,7 @@
     <ClCompile>
       <WarningLevel>Level3</WarningLevel>
       <SDLCheck>true</SDLCheck>
-      <PreprocessorDefinitions>_DEBUG;VERSION_EXPORTS;_WINDOWS;_USRDLL;WIN32_LEAN_AND_MEAN;NOMINMAX;NOGDI;NOSERVICE;%(PreprocessorDefinitions)</PreprocessorDefinitions>
+      <PreprocessorDefinitions>_DEBUG;VERSION_EXPORTS;_WINDOWS;_USRDLL;WIN32_LEAN_AND_MEAN;NOGDI;NOMINMAX;NOSERVICE;%(PreprocessorDefinitions)</PreprocessorDefinitions>
       <ConformanceMode>true</ConformanceMode>
       <PrecompiledHeader>NotUsing</PrecompiledHeader>
       <PrecompiledHeaderFile>pch.h</PrecompiledHeaderFile>
@@ -174,7 +174,7 @@
       <FunctionLevelLinking>true</FunctionLevelLinking>
       <IntrinsicFunctions>true</IntrinsicFunctions>
       <SDLCheck>true</SDLCheck>
-      <PreprocessorDefinitions>NDEBUG;VERSION_EXPORTS;_WINDOWS;_USRDLL;WIN32_LEAN_AND_MEAN;NOMINMAX;NOGDI;NOSERVICE;%(PreprocessorDefinitions)</PreprocessorDefinitions>
+      <PreprocessorDefinitions>NDEBUG;VERSION_EXPORTS;_WINDOWS;_USRDLL;WIN32_LEAN_AND_MEAN;NOGDI;NOMINMAX;NOSERVICE;%(PreprocessorDefinitions)</PreprocessorDefinitions>
       <ConformanceMode>true</ConformanceMode>
       <PrecompiledHeader>NotUsing</PrecompiledHeader>
       <PrecompiledHeaderFile>pch.h</PrecompiledHeaderFile>
@@ -209,6 +209,7 @@
     <ClCompile Include="include\SafetyHook\Zydis.c" />
     <ClCompile Include="include\ScreenCleaner\ScreenCleaner.cpp" />
     <ClCompile Include="src\base.cpp" />
+    <ClCompile Include="src\misc\logger.cpp" />
     <ClCompile Include="src\ui\menu.cpp" />
     <ClCompile Include="src\ui\overlay.cpp" />
   </ItemGroup>
@@ -229,6 +230,7 @@
     <ClInclude Include="src\misc\fonts\font_awesome.hpp" />
     <ClInclude Include="src\misc\fonts\roboto_mono.hpp" />
     <ClInclude Include="src\misc\keybinds.h" />
+    <ClInclude Include="src\misc\logger.h" />
     <ClInclude Include="src\ui\backend\D3D11.h" />
     <ClInclude Include="src\ui\backend\D3D12.h" />
     <ClInclude Include="src\ui\backend\Discord.h" />

version.vcxproj.filters
@@ -26,6 +26,9 @@
     <ClCompile Include="include\ScreenCleaner\ScreenCleaner.cpp">
       <Filter>include\ScreenCleaner</Filter>
     </ClCompile>
+    <ClCompile Include="src\misc\logger.cpp">
+      <Filter>src\misc</Filter>
+    </ClCompile>
   </ItemGroup>
   <ItemGroup>
     <ClInclude Include="resource.h" />
@@ -91,6 +94,9 @@
     </ClInclude>
     <ClInclude Include="src\ui\backend\Vulkan.h">
       <Filter>src\ui\backend</Filter>
+    </ClInclude>
+    <ClInclude Include="src\misc\logger.h">
+      <Filter>src\misc</Filter>
     </ClInclude>
   </ItemGroup>
   <ItemGroup>

commit 423ec74da08e316fb57c1a4e3b265d97c885c9f0

fix: Added missing #include on base.cpp Caused by commit 691be77

src/base.cpp
@@ -4,6 +4,7 @@
 #include <SafetyHook/Wrapper.hpp>
 #include <ScreenCleaner/ScreenCleaner.h>
 
+#include "game.h"
 #include "hooks.h"
 #include "misc/logger.h"
 #include "ui/overlay.h"