7dfc2c6b6473732c996a049aab4624a3ff2dd557
Leonardo V.A.C. <leonardovac@outlook.com.br>
2025-07-14 10:10:51 -0300

    feat: Add my own VMT Hooking lib.

---
+++ include/VMTHook/vmthook.cpp
@@ -0,0 +1,82 @@
+﻿#include "vmthook.h"
+
+#include <windows.h>
+
+// Static member definition
+std::unordered_map<const void*, VMTHook::OriginalMethod> VMTHook::mOriginalMethods;
+VMTHook::OriginalMethod VMTHook::nullMethod;
+
+namespace
+{
+    void Patch(void** address, const uint32_t index, void* value)
+    {
+        DWORD oldProtection;
+        VirtualProtect(static_cast<void*>(address + index), sizeof(uintptr_t), PAGE_EXECUTE_READWRITE, &oldProtection);
+        address[index] = value;
+        VirtualProtect(static_cast<void*>(address + index), sizeof(uintptr_t), oldProtection, &oldProtection);
+    }
+}
+
+void* VMTHook::Hook(const uint32_t index, void* newMethod)
+{
+    if (!newMethod || index >= tableSize || mHookedMethods.contains(index)) return nullptr;
+
+    const auto originalMethod = pVTable[index];
+    mOriginalMethods[newMethod] = originalMethod;
+    Patch(pVTable, index, newMethod);
+    mHookedMethods[index] = originalMethod;
+    return originalMethod;
+}
+
+bool VMTHook::Unhook(const uint32_t index)
+{
+    if (index >= tableSize || !mHookedMethods.contains(index)) return false;
+
+    const auto currentMethod = pVTable[index];
+    Patch(pVTable, index, mHookedMethods[index]);
+    mOriginalMethods.erase(currentMethod);
+    mHookedMethods.erase(index);
+    return true;
+}
+
+void VMTHook::UnhookAll()
+{
+    for (const auto& [index, originalMethod] : mHookedMethods)
+    {
+	    if (const auto currentMethod = pVTable[index]; currentMethod != originalMethod) // Only patch if currently hooked
+        {
+            Patch(pVTable, index, originalMethod);
+            mOriginalMethods.erase(currentMethod);
+        }
+    }
+    mHookedMethods.clear();
+}
+
+VMTHook::OriginalMethod& VMTHook::GetOriginal(const void* hookFunction)
+{
+    if (const auto it = mOriginalMethods.find(hookFunction); it != mOriginalMethods.end())
+    {
+        return it->second;
+    }
+    return nullMethod;
+}
+
+uint32_t VMTHook::GetVTableSize() const
+{
+    uint32_t index = 0;
+    MEMORY_BASIC_INFORMATION mbiBuffer = {}; //buffer to store the information of some memory region
+    while (true)
+    {
+        if (!pVTable[index] || !VirtualQuery(pVTable[index], &mbiBuffer, sizeof(mbiBuffer)))
+        {
+            break;
+        }
+
+        if (mbiBuffer.State != MEM_COMMIT || mbiBuffer.Protect & (PAGE_GUARD | PAGE_NOACCESS) || !(mbiBuffer.Protect & (PAGE_EXECUTE | PAGE_EXECUTE_READ | PAGE_EXECUTE_READWRITE)))
+        {
+            break;
+        }
+        ++index;
+    }
+    return index;
+}
---
+++ include/VMTHook/vmthook.h
@@ -0,0 +1,61 @@
+﻿#pragma once
+#include <unordered_map>
+
+class VMTHook
+{
+public:
+    VMTHook() = default;
+    template<typename T>
+    explicit VMTHook(T* vTable) : pVTable(reinterpret_cast<void**>(vTable)), tableSize(GetVTableSize()) {}
+    ~VMTHook() { UnhookAll(); }
+    void* Hook(uint32_t index, void* newMethod);
+    bool Unhook(uint32_t index);
+    void UnhookAll();
+
+    // Type-safe original function calls
+    class OriginalMethod
+    {
+    public:
+        OriginalMethod() = default;
+        OriginalMethod(void* originalPtr) : pOriginal(originalPtr) {}
+
+        template<typename ReturnType = void, typename... Args>
+        ReturnType call(Args... args) const
+        {
+            return reinterpret_cast<ReturnType(*)(Args...)>(pOriginal)(args...);
+        }
+
+        template<typename ReturnType = void, typename... Args>
+        ReturnType stdcall(Args... args) const
+        {
+            return reinterpret_cast<ReturnType(__stdcall*)(Args...)>(pOriginal)(args...);
+        }
+
+        template<typename ReturnType = void, typename... Args>
+        ReturnType thiscall(Args... args) const
+        {
+            return reinterpret_cast<ReturnType(__thiscall*)(Args...)>(pOriginal)(args...);
+        }
+
+        [[nodiscard]] bool isValid() const { return pOriginal != nullptr; }
+
+    private:
+        void* pOriginal;
+    };
+
+    // Static function to get originalMethod using hook function pointer
+    static OriginalMethod& GetOriginal(const void* hookFunction);
+
+private:
+    void** pVTable;
+    uint32_t tableSize;
+
+    // Store original functions for 'index -> originalMethod'
+    std::unordered_map<uint32_t, void*> mHookedMethods;
+
+    // Static storage for 'newMethod -> originalMethod' mapping
+    static std::unordered_map<const void*, OriginalMethod> mOriginalMethods;
+    static OriginalMethod nullMethod;
+
+    [[nodiscard]] uint32_t GetVTableSize() const;
+};
