9aca872c4a4fec6096b4623da6299814de5a0e07
Leonardo V.A.C. <leonardovac@outlook.com.br>
2025-07-30 16:44:27 -0300

    refactor: Enforce correct usage of TinyHook's Setup templates with concepts.

--- include/TinyHook/tinyhook.h
+++ include/TinyHook/tinyhook.h
@@ -5,25 +5,34 @@
 
 namespace TinyHook
 {
-	template <typename Type, typename T>
+	template<typename T>
+	concept tiny_hook = std::is_same_v<T, EATHook> || std::is_same_v<T, IATHook> || std::is_same_v<T, VMTHook>;
+
+	template<typename T>
+	concept at_hook = std::is_same_v<T, EATHook> || std::is_same_v<T, IATHook>;
+
+	template<typename T>
+	concept vmt_hook = std::is_same_v<T, VMTHook>;
+
+	template <at_hook Type, typename T>
 	[[nodiscard]] std::unique_ptr<Type> Setup(void* module, std::string_view name = {})
 	{
 		return std::make_unique<Type>(module, name);
 	}
 
-	template <typename Type, typename T>
+	template <at_hook Type, typename T>
 	[[nodiscard]] std::unique_ptr<Type> Setup(const char* moduleName)
 	{
 		return std::make_unique<Type>(moduleName);
 	}
 
-	template <typename Type, typename T>
+	template <at_hook Type, typename T>
 	[[nodiscard]] std::unique_ptr<Type> Setup(const wchar_t* moduleName)
 	{
 		return std::make_unique<Type>(moduleName);
 	}
 
-	template <typename Type, typename T>
+	template <vmt_hook Type, typename T>
 	[[nodiscard]] std::unique_ptr<Type> Setup(T* vTable, std::string_view name = {})
 	{
 		return std::make_unique<Type>(vTable, name);
