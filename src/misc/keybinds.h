615a7e95a320c0fcfe5c0fbf6b8d18ebb8c894ce
Leonardo V.A.C. <leonardovac@outlook.com.br>
2025-08-28 21:15:39 -0300

    refactor: Improve KeyBind widget

--- src/misc/keybinds.h
+++ src/misc/keybinds.h
@@ -2,7 +2,6 @@
 #ifndef KEYBINDS_H
 #define KEYBINDS_H
 #include <algorithm>
-#include <format>
 #include <string>
 #include <unordered_map>
 #include <vector>
@@ -20,14 +19,14 @@ namespace Keybinds
 		{
 			std::string output = input;
 
-			std::ranges::transform(output, output.begin(), [](unsigned char c)
+			std::ranges::transform(output, output.begin(), [](const unsigned char c)
 				{
 					return std::tolower(c);
 				});
 
 			std::ranges::replace(output, ' ', '_');
 
-			std::erase_if(output, [](unsigned char c)
+			std::erase_if(output, [](const unsigned char c)
 				{
 					return !(std::isalnum(c) || c == '_');
 				});
@@ -54,6 +53,11 @@ namespace Keybinds
 		{
 			return static_cast<std::uint16_t>(ImGui::GetIO().KeyMods);
 		}
+	}
+
+	inline std::string GetKeyIcon(const int key)
+	{
+		return key >= ImGuiKey_MouseLeft && key <= ImGuiKey_MouseWheelY ? ICON_FA_COMPUTER_MOUSE : ICON_FA_KEYBOARD;
 	}
 
 	inline std::string GetKeyName(const int key, const std::uint16_t modifiers)
@@ -102,7 +106,7 @@ namespace Keybinds
 			TOGGLE,
 			HOLD
 		};
-		inline static float gui_spacing {};
+		inline static float button_size {};
 
 		std::string		name;
 		bool*			pFunction;
@@ -110,11 +114,11 @@ namespace Keybinds
 		Type			type{ TOGGLE };
 		std::uint16_t	modifiers{ 0 };
 
-		std::string		keyName;
+		std::string		keyName{ "NONE" };
 		std::string		keyIcon{ ICON_FA_KEYBOARD };
 		bool			isWaiting{ false };
 
-		KeyBind(std::string name, bool* function, const std::uint16_t key = 0, const Type type = TOGGLE, const std::uint16_t modifiers = 0) : name(std::move(name)), pFunction(function), key(key), type(type), modifiers(modifiers), keyName(GetKeyName(key, modifiers)) {}
+		KeyBind(std::string name, bool* function, const std::uint16_t key = 0, const Type type = TOGGLE, const std::uint16_t modifiers = 0) : name(std::move(name)), pFunction(function), key(key), type(type), modifiers(modifiers), keyName(GetKeyName(key, modifiers)), keyIcon(GetKeyIcon(key)) {}
 
         void ChangeType()
         {
@@ -148,21 +152,22 @@ namespace Keybinds
 			key = pressedKey;
 			modifiers = currentModifiers;
 			keyName = GetKeyName(key, modifiers);
+			keyIcon = GetKeyIcon(pressedKey);
 			isWaiting = false;
-			gui_spacing = 0;
+			button_size = 0;
 		}
 
 		void Clear() { Update(0, 0); }
 	};
 
-	inline std::vector<KeyBind> kbList
+	inline std::vector<KeyBind> bindsList
 	{
 		{"open_menu", &Menu::bOpen, ImGuiKey_Insert},
 	};
 
 	inline KeyBind* GetKeyBind(const bool* function)
 	{
-		for (KeyBind& keyBind : kbList)
+		for (KeyBind& keyBind : bindsList)
 		{
 			if (keyBind.pFunction == function)
 			{
@@ -175,7 +180,7 @@ namespace Keybinds
 	inline void CheckKeybinds()
 	{
 		const std::uint16_t currentModifiers = static_cast<std::uint16_t>(ImGui::GetIO().KeyMods);
-		for (KeyBind& keyBind : kbList)
+		for (KeyBind& keyBind : bindsList)
 		{
 			keyBind.Check(currentModifiers);
 		}
@@ -186,46 +191,83 @@ namespace ImGui
 {
 	inline bool CustomBindKey(const char* label, Keybinds::KeyBind* keyBind, const bool useCheckbox = false)
 	{
-		const char* typeText = useCheckbox ? "" : keyBind->type == Keybinds::KeyBind::TOGGLE ? "[T] " : "[H] ";
-		const std::string visibleText = std::format("{}{} Key: {}", typeText, label, keyBind->keyName);
 		const std::string labelText = std::string("##") + label;
-		const float itemSize = ImGui::CalcTextSize(visibleText.c_str()).x;
+		const float itemSize = ImGui::CalcTextSize(label).x;
 
 		if (useCheckbox && keyBind->pFunction != nullptr)
 		{
-			Checkbox(visibleText.c_str(), keyBind->pFunction);
+			Checkbox(label, keyBind->pFunction);
 		}
 		else
 		{
 			ImGui::AlignTextToFramePadding();
 			ImGui::SetNextItemWidth(itemSize);
-			ImGui::TextUnformatted(visibleText.c_str());
+			ImGui::TextUnformatted(label);
 			if (ImGui::IsItemClicked() && ImGui::GetMouseClickedCount(0) == 2)
 			{
 				keyBind->ChangeType();
 			}
 		}
 
-		Keybinds::KeyBind::gui_spacing = std::max(Keybinds::KeyBind::gui_spacing, itemSize);
-		ImGui::SameLine(ImGui::GetCursorPosX() + ImGui::GetStyle().ItemSpacing.x + Keybinds::KeyBind::gui_spacing, 1 * ImGui::GetStyle().ItemSpacing.x);
+		const std::string buttonText = keyBind->keyIcon + " " + (keyBind->isWaiting ? "Press any key..." : keyBind->keyName);
+		const std::string buttonLabel = buttonText + labelText;
+		ImVec2 totalSize{ ImGui::CalcTextSize((buttonText + "  ").c_str()).x + ImGui::GetStyle().ItemSpacing.x, ImGui::GetFrameHeight() };
 
-		keyBind->keyIcon = keyBind->key >= ImGuiKey_MouseLeft && keyBind->key <= ImGuiKey_MouseWheelY ? ICON_FA_COMPUTER_MOUSE : ICON_FA_KEYBOARD;
-		const char* buttonText = keyBind->isWaiting ? "Press any key..." : "Bind Key";
-		const std::string buttonLabel = keyBind->keyIcon + " " + buttonText + labelText;
+		if (!keyBind->isWaiting)
+		{
+			Keybinds::KeyBind::button_size = std::max(Keybinds::KeyBind::button_size, totalSize.x);
+			totalSize.x = Keybinds::KeyBind::button_size;
+		}
 
-		if (ImGui::Button(buttonLabel.c_str()))
+		ImGui::SameLine(ImGui::GetContentRegionAvail().x - totalSize.x + ImGui::GetCursorPosX());
+
+		const ImVec2 pos = ImGui::GetCursorScreenPos();
+		constexpr float xButtonSize = 16.0f;
+
+		// Main button
+		const ImVec2 mainSize{ totalSize.x - xButtonSize, totalSize.y };
+		if (ImGui::InvisibleButton(labelText.c_str(), mainSize))
 		{
 			keyBind->isWaiting = !keyBind->isWaiting;
 		}
 
+		/// Start of custom drawings
+		ImDrawList* drawList = ImGui::GetWindowDrawList();
+		const ImU32 bgColor = ImGui::IsItemHovered() ? ImGui::GetColorU32(ImGuiCol_FrameBgHovered) : keyBind->isWaiting ? ImGui::GetColorU32(ImGuiCol_FrameBgActive) : ImGui::GetColorU32(ImGuiCol_FrameBg);
+		const ImGuiStyle style = ImGui::GetStyle();
+
+		// Draw background
+		drawList->AddRectFilled(pos, ImVec2(pos.x + totalSize.x, pos.y + totalSize.y), bgColor, style.FrameRounding);
+		// Draw border
+		drawList->AddRect(pos, ImVec2(pos.x + totalSize.x, pos.y + totalSize.y), ImGui::GetColorU32(ImGuiCol_Border), style.FrameRounding, 0, style.FrameBorderSize);
+
+		// Draw main text
+		const ImVec2 textSize = ImGui::CalcTextSize(buttonText.c_str());
+		const ImVec2 textPos{ pos.x + 8.0f, pos.y + (totalSize.y - textSize.y) * 0.5f };
+		drawList->AddText(textPos, ImGui::GetColorU32(ImGuiCol_Text), buttonText.c_str());
+
 		if (keyBind->key)
 		{
-			ImGui::SameLine();
-			if (ImGui::Button((std::string(ICON_FA_ERASER" Clear") + labelText).c_str()))
+			// Clear button
+			ImGui::SameLine(0, 0);
+			const ImVec2 xPos = ImGui::GetCursorScreenPos();
+			if (ImGui::InvisibleButton((labelText + "_clear").c_str(), ImVec2(xButtonSize, totalSize.y)))
 			{
 				keyBind->Clear();
 			}
+			const bool xHovered = ImGui::IsItemHovered();
+
+			// Draw separator line
+			drawList->AddLine(ImVec2(pos.x + mainSize.x, pos.y + 2), ImVec2(pos.x + mainSize.x, pos.y + totalSize.y - 2), ImGui::GetColorU32(ImGuiCol_TextDisabled), 1.0f);
+
+			// Draw X
+			const ImU32 xColor = xHovered ? IM_COL32(255, 100, 100, 255) : IM_COL32(150, 150, 150, 255);
+			const ImVec2 xCenter{ xPos.x + xButtonSize * 0.5f, xPos.y + totalSize.y * 0.5f };
+			constexpr float xSize = 4.0f;
+			drawList->AddLine(ImVec2(xCenter.x - xSize, xCenter.y - xSize), ImVec2(xCenter.x + xSize, xCenter.y + xSize), xColor, 2.0f);
+			drawList->AddLine(ImVec2(xCenter.x + xSize, xCenter.y - xSize), ImVec2(xCenter.x - xSize, xCenter.y + xSize), xColor, 2.0f);
 		}
+		/// End of custom drawings
 
 		if (keyBind->isWaiting)
 		{
@@ -236,7 +278,8 @@ namespace ImGui
 				const bool isModifierKey = key >= ImGuiKey_LeftCtrl && key <= ImGuiKey_Menu;
 				if ((isModifierKey && ImGui::IsKeyReleased(static_cast<ImGuiKey>(key))) || (!isModifierKey && ImGui::IsKeyPressed(static_cast<ImGuiKey>(key))))
 				{
-					keyBind->Update(key, Keybinds::Modifiers::GetModifiers());
+					if (key == ImGuiKey_Escape) keyBind->Clear();
+					else keyBind->Update(key, Keybinds::Modifiers::GetModifiers());
 					break;
 				}
 			}
@@ -254,8 +297,8 @@ namespace ImGui
 			return CustomBindKey(label, existingKeyBind, useCheckbox);
 		}
 
-		Keybinds::kbList.emplace_back(Keybinds::Utils::ToSnakeCase(label), pFunction);
-		return CustomBindKey(label, &Keybinds::kbList.back(), useCheckbox);
+		Keybinds::bindsList.emplace_back(Keybinds::Utils::ToSnakeCase(label), pFunction);
+		return CustomBindKey(label, &Keybinds::bindsList.back(), useCheckbox);
 	}
 
 	inline bool CustomBindKey(const char* label, const Keybinds::KeyBind& keyBind, const bool useCheckbox = false)
@@ -267,8 +310,8 @@ namespace ImGui
 			return CustomBindKey(label, existingKeyBind, useCheckbox);
 		}
 
-		Keybinds::kbList.push_back(keyBind);
-		return CustomBindKey(label, &Keybinds::kbList.back(), useCheckbox);
+		Keybinds::bindsList.push_back(keyBind);
+		return CustomBindKey(label, &Keybinds::bindsList.back(), useCheckbox);
 	}
 }
  #endif
